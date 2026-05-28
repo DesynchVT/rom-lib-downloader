43b45de6785302484ccac733b2d905e8b8beadf2
DesynchVT <desynchvt@gmail.com>
2026-05-28 20:24:35 +0200

    Re-did some data structures

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use color_eyre::eyre::Result;
 use crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
@@ -12,8 +13,14 @@ use ratatui::{
 use crate::des_ssh;
 
 #[derive(Debug, Default)]
+struct RomItem {
+    rom_title: String,
+    console: String,
+    is_selected: bool,
+}
+
+#[derive(PartialEq)]
 enum AppMode {
-    #[default]
     SelectConsole,
     SelectRom,
     SelectSelection,
@@ -22,15 +29,60 @@ enum AppMode {
     Confirming,
 }
 
-#[derive(Debug, Default)]
-struct AppState {
+struct AppState<'a> {
+    sess: &'a ssh2::Session,
     consoles_list: Vec<String>,
-    roms_list: Vec<String>,
+    roms_list: Vec<RomItem>,
     highlighted_console: ListState,
     highlighted_rom: ListState,
-    selected_roms: HashMap<String, Vec<String>>,
+    selected_roms: HashMap<String, Vec<RomItem>>,
     mode: AppMode,
     current_console: String,
+}
+
+impl<'a> AppState<'a> {
+    fn new(sess: &'_ ssh2::Session, consoles_list: Vec<String>) -> AppState<'_> {
+        AppState {
+            sess,
+            consoles_list,
+            roms_list: vec![],
+            highlighted_console: ListState::default(),
+            highlighted_rom: ListState::default(),
+            selected_roms: HashMap::default(),
+            mode: AppMode::SelectConsole,
+            current_console: String::new(),
+        }
+    }
+
+    fn set_mode(&mut self, new_mode: AppMode) {
+        match new_mode {
+            AppMode::SelectConsole => {
+                self.mode = AppMode::SelectConsole;
+                self.current_console = String::new();
+            }
+            AppMode::SelectRom => {
+                self.mode = AppMode::SelectRom;
+                let selected_index = self.highlighted_console.selected().unwrap();
+                self.current_console = self.consoles_list[selected_index].clone();
+
+                let existing_roms = des_ssh::list_roms(self.sess, &self.current_console);
+
+                self.roms_list = existing_roms
+                    .iter()
+                    .map(|r| RomItem {
+                        rom_title: r.clone(),
+                        console: self.current_console.clone(),
+                        is_selected: false,
+                    })
+                    .collect();
+                self.highlighted_rom.select_first();
+            }
+            AppMode::Quitting => {
+                self.mode = AppMode::Quitting;
+            }
+            _ => {}
+        }
+    }
 }
 
 pub fn main(sess: &ssh2::Session, consoles_list: Vec<String>) -> color_eyre::Result<()> {
@@ -47,17 +99,15 @@ fn app(
     sess: &ssh2::Session,
     consoles_list: Vec<String>,
 ) -> color_eyre::Result<()> {
-    let mut app_state = AppState {
-        consoles_list,
-        ..Default::default()
-    };
+    let mut app_state = AppState::new(sess, consoles_list);
+
     app_state.highlighted_console.select_first();
-    loop {
+    while app_state.mode != AppMode::Quitting {
         terminal.draw(|frame| render(frame, &mut app_state))?;
 
         if let Some(key) = event::read()?.as_key_press_event() {
             match key.code {
-                KeyCode::Char('q') | KeyCode::Esc => break Ok(()),
+                KeyCode::Char('q') | KeyCode::Esc => app_state.set_mode(AppMode::Quitting),
                 KeyCode::Char('k') | KeyCode::Up => match app_state.mode {
                     AppMode::SelectConsole => app_state.highlighted_console.select_previous(),
                     AppMode::SelectRom => app_state.highlighted_rom.select_previous(),
@@ -71,27 +121,24 @@ fn app(
                     _ => {}
                 },
                 KeyCode::Char('l') | KeyCode::Enter => match app_state.mode {
-                    AppMode::SelectConsole => {
-                        app_state.mode = AppMode::SelectRom;
-                        let selected_index = app_state.highlighted_console.selected().unwrap();
-                        app_state.current_console = app_state.consoles_list[selected_index].clone();
-
-                        app_state.roms_list = des_ssh::list_roms(sess, &app_state.current_console);
-                        app_state.highlighted_rom.select_first();
-                    }
+                    AppMode::SelectConsole => app_state.set_mode(AppMode::SelectRom),
                     AppMode::SelectSelection => {}
                     _ => {}
                 },
                 KeyCode::Char('h') | KeyCode::Backspace => match app_state.mode {
                     AppMode::SelectRom => {
-                        app_state.mode = AppMode::SelectConsole;
-                        app_state.current_console = String::new();
+                        app_state.set_mode(AppMode::SelectConsole);
                     }
-                    AppMode::SelectSelection => {}
+                    AppMode::SelectSelection => {
+                        app_state.set_mode(AppMode::SelectConsole);
+                    }
                     _ => {}
                 },
                 KeyCode::Char(' ') => match app_state.mode {
                     AppMode::SelectRom => {
+                        app_state.mode = AppMode::SelectConsole;
+                    }
+                    AppMode::SelectSelection => {
                         todo!();
                     }
                     _ => {}
@@ -100,6 +147,7 @@ fn app(
             }
         }
     }
+    Ok(())
 }
 
 fn render(frame: &mut Frame, app_state: &mut AppState) {
@@ -146,8 +194,12 @@ fn render(frame: &mut Frame, app_state: &mut AppState) {
     } else {
         format!(" {} ", &app_state.current_console)
     };
+    let mut roms: Vec<ListItem> = vec![];
+    for rom in &app_state.roms_list {
+        roms.push(ListItem::from(rom.rom_title.clone()));
+    }
     frame.render_stateful_widget(
-        List::new(app_state.roms_list.clone())
+        List::new(roms)
             .block(
                 Block::default()
                     .title_top(Line::from(" ROMS ").centered().bold())
